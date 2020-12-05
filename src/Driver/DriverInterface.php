4ebc24bcfa69b49354be543019e9b07fe2378d84
Francerz <francerzam@hotmail.com>
2020-12-05 12:33:37 -0600

    Added ExecuteQueryExceptions
    Added ExecuteQueryException
    Added ExecuteSelectException
    Added ExecuteInsertException
    Added ExecuteUpdateException
    Added ExecuteDeleteException

--- src/Driver/DriverInterface.php
+++ src/Driver/DriverInterface.php
@@ -6,7 +6,6 @@ use Francerz\SqlBuilder\CompiledQuery;
 use Francerz\SqlBuilder\ConnectParams;
 use Francerz\SqlBuilder\Results\DeleteResult;
 use Francerz\SqlBuilder\Results\InsertResult;
-use Francerz\SqlBuilder\Results\QueryResultInterface;
 use Francerz\SqlBuilder\Results\SelectResult;
 use Francerz\SqlBuilder\Results\UpdateResult;
 
@@ -16,8 +15,43 @@ interface DriverInterface
     public function getCompiler() : ?QueryCompilerInterface;
     public function getTranslator() : ?QueryTranslatorInterface;
     
+    /**
+     * Executes given SELECT CompiledQuery and returns the result.
+     *
+     * @param CompiledQuery $query Compiled SELECT query.
+     * @return SelectResult
+     * 
+     * @throws ExecuteSelectException
+     */
     public function executeSelect(CompiledQuery $query) : SelectResult;
+
+    /**
+     * Executes given INSERT CompiledQuery and returns the result.
+     *
+     * @param CompiledQuery $query Compiled INSERT query.
+     * @return InsertResult
+     * 
+     * @throws ExecuteInsertException
+     */
     public function executeInsert(CompiledQuery $query) : InsertResult;
+
+    /**
+     * Executees given UPDATE CompiledQuery and returns the result.
+     *
+     * @param CompiledQuery $query
+     * @return UpdateResult
+     * 
+     * @throws ExecuteUpdateException
+     */
     public function executeUpdate(CompiledQuery $query) : UpdateResult;
+
+    /**
+     * Executes given DELETE CompiledQuery and returns the result.
+     *
+     * @param CompiledQuery $query
+     * @return DeleteResult
+     * 
+     * @throws ExecuteDeleteException
+     */
     public function executeDelete(CompiledQuery $query) : DeleteResult;
 }
---
+++ src/Exceptions/ExecuteQueryException.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Francerz\SqlBuilder\Exceptions;
+
+use Exception;
+use Francerz\SqlBuilder\CompiledQuery;
+use Throwable;
+
+class ExecuteQueryException extends Exception
+{
+    private $compiledQuery;
+
+    public function __construct(CompiledQuery $compiledQuery, string $message = "", int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->compiledQuery = $compiledQuery;
+    }
+
+    public function getCompiledQuery()
+    {
+        return $this->compiledQuery;
+    }
+
+    public function getQueryString()
+    {
+        return $this->compiledQuery->getQuery();
+    }
+
+    public function getQueryObject()
+    {
+        return $this->compiledQuery->getObject();
+    }
+
+    public function getValues()
+    {
+        return $this->compiledQuery->getValues();
+    }
+}
+
+class ExecuteSelectException extends ExecuteQueryException
+{
+
+}
+
+class ExecuteInsertException extends ExecuteQueryException
+{
+
+}
+
+class ExecuteUpdateException extends ExecuteQueryException
+{
+
+}
+
+class ExecuteDeleteException extends ExecuteQueryException
+{
+    
+}
