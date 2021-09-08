76af23e17e34c568896488f7ea5d4449edf34da7
Francerz <francerzam@hotmail.com>
2021-09-08 16:40:27 -0500

    RBK disabled query optimizer

--- src/DatabaseHandler.php
+++ src/DatabaseHandler.php
@@ -94,7 +94,7 @@ class DatabaseHandler
 
     public function executeSelect(SelectQuery $query) : SelectResult
     {
-        $query = QueryOptimizer::optimizeSelect($query);
+        // $query = git QueryOptimizer::optimizeSelect($query);
         $compiled = $this->prepareQuery($query);
         $result = $this->driver->executeSelect($compiled);
 
